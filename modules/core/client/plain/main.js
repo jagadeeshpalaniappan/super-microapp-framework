ffd185aacd002497a42556b4db498fc0289cbffe
Jagadeesh <jagadeeshthegeek@gmail.com>
2017-06-17 00:42:50 -0700

    sidemenu impln --plainjs

---
+++ modules/core/client/plain/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+
+  // *************** Side Menu *******************
+
+  $("#sideMenuBtn").click(function () {
+    $("body").toggleClass("sidemenu-minimized");
+    $(".sidemenu .hide-on-small-menu").toggle();
+  });
+
+  // *************** / Side Menu ******************
+
+});
